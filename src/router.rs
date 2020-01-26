ca031b2970a6c694137c142d5b2abc192be919cd
misonomi <misonomi0612@gmail.com>
2020-01-26 15:33:38 +0900

    url decode string

--- src/router.rs
+++ src/router.rs
@@ -1,6 +1,7 @@
 use rocket::{get, State, http::RawStr};
 use rocket_contrib::json::Json;
 use egg_mode::search::{self, ResultType};
+use percent_encoding::{percent_decode_str};
 
 use tokio::runtime::current_thread::block_on_all;
 use super::twitter::{make_query, shoud_display, print_tweet};
@@ -13,11 +14,16 @@ pub fn hello() -> &'static str {
 
 #[get("/search/<terms>")]
 pub fn search(tw_token: State<egg_mode::Token>, terms: &RawStr) -> Json<Vec<TweetStub>> {
-    let query_vec = terms.as_str()
+    let terms_decoded: String;
+    match percent_decode_str(terms).decode_utf8() {
+        Ok(v) => terms_decoded = v.to_string(),
+        Err(_) => return Json(vec![]),
+    }
+    let terms_vec = terms_decoded
         .split('+')
         .collect::<Vec<&str>>();
     let search = block_on_all(
-        search::search(make_query(query_vec.clone()))
+        search::search(make_query(terms_vec.clone()))
             .result_type(ResultType::Recent)
             .count(100)
             .call(&tw_token)
@@ -29,7 +35,7 @@ pub fn search(tw_token: State<egg_mode::Token>, terms: &RawStr) -> Json<Vec<Twee
     Json(
         search.statuses.clone()
             .into_iter()
-            .filter(|t| shoud_display(t, &query_vec))
+            .filter(|t| shoud_display(t, &terms_vec))
             .map(|t| TweetStub::from(t))
             .collect()
     )
